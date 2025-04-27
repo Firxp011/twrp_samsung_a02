f45577247bc93a0f9d3b2de715ec571664e2d472
FIRxp011 <113378946+Firxp011@users.noreply.github.com>
2025-04-27 19:02:41 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,6 +98,8 @@ PLATFORM_VERSION := 16.1.0
 # TWRP Configuration
 TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
+
+BOARD_RAMDISK_COMPRESS_LZMA := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
